f6f7190f0c6a7d1dc0454c22b171233a71d588c1
Yan Xi <tanyanxi21@gmail.com>
2019-04-03 17:09:51 +0000

    Created new contacts tbale in attempt to solve problem

---
+++ db/migrate/20190403170837_create_contractts.rb
@@ -0,0 +1,6 @@
+class CreateContractts < ActiveRecord::Migration[5.0]
+  def change
+    create_table :contractts do |t|
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190303070348) do
+ActiveRecord::Schema.define(version: 20190403170837) do
 
-  create_table "contacts", force: :cascade do |t|
+  create_table "contactts", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.text     "comments"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "contractts", force: :cascade do |t|
   end
 
 end
